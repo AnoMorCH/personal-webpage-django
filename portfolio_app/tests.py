d569eda495e604ad55b0761b526bf7acd12bd17b
AnoMorCH <anomorch@gmail.com>
2023-07-27 10:54:47 +0300

    change naming convention of tests.py

--- portfolio_app/tests.py
+++ portfolio_app/tests.py
@@ -24,41 +24,37 @@ class CurrentStaticPathTests(TestCase):
     }
 
     def test_private_get_path(self):
-        actual_path = self.current_static_path._CurrentStaticPath__get_path()
-        self.assertEqual(self.expected_path, actual_path)
-        self.assertEqual(type(actual_path), str)
+        actual_value = self.current_static_path._CurrentStaticPath__get_path()
+        self.assertEqual(self.expected_path, actual_value)
+        self.assertEqual(type(actual_value), str)
 
     def test_private_get_response(self):
-        actual_response = self.current_static_path._CurrentStaticPath__get_response()
-        self.assertEqual(self.expected_response["path"],
-                         actual_response["path"])
-        self.assertEqual(self.expected_response["status"],
-                         actual_response["status"])
-        self.assertEqual(type(actual_response), dict)
+        actual_value = self.current_static_path._CurrentStaticPath__get_response()
+        self.assertEqual(self.expected_response["path"], actual_value["path"])
+        self.assertEqual(self.expected_response["status"], actual_value["status"])
+        self.assertEqual(type(actual_value), dict)
 
     def test_get_json_response(self):
-        expected_json_response = json.dumps(self.expected_response, indent=4)
-        actual_json_response = self.current_static_path.get_json_response()
-        self.assertEqual(expected_json_response, actual_json_response)
-        self.assertEqual(type(expected_json_response),
-                         type(actual_json_response))
+        expected_value = json.dumps(self.expected_response, indent=4)
+        actual_value = self.current_static_path.get_json_response()
+        self.assertEqual(expected_value, actual_value)
+        self.assertEqual(type(expected_value), type(actual_value))
 
 
 class ServicesTests(TestCase):
     app_name = "portfolio_app"
 
     def test_get_current_app_name(self):
-        actual_app_name = get_current_app_name()
-        self.assertEqual(self.app_name, actual_app_name)
-        self.assertEqual(type(self.app_name), type(actual_app_name))
+        actual_value = get_current_app_name()
+        self.assertEqual(self.app_name, actual_value)
+        self.assertEqual(type(self.app_name), type(actual_value))
 
     def test_get_full_static_path(self):
-        expected_full_static_path = str(BASE_DIR) + "/" + self.app_name \
-            + STATIC_URL + self.app_name
-        actual_full_static_path = get_full_static_path(self.app_name)
-        self.assertEqual(expected_full_static_path, actual_full_static_path)
-        self.assertEqual(type(expected_full_static_path),
-                         type(actual_full_static_path))
+        expected_value = str(BASE_DIR) + "/" + self.app_name + STATIC_URL \
+            + self.app_name
+        actual_value = get_full_static_path(self.app_name)
+        self.assertEqual(expected_value, actual_value)
+        self.assertEqual(type(expected_value), type(actual_value))
 
     def test_get_files_amount_inside_folder(self):
         test_path = os.getcwd() + "/test/"
